299cbf6e5c1b72266d4c83d7bac4f3bc3d68dcd6
aurorapuli <144793745+aurorapuli@users.noreply.github.com>
2023-12-22 15:42:32 +0100

    config: cards

---
+++ src/components/Cards.vue
@@ -0,0 +1,41 @@
+<script>
+
+
+export default {
+    name: "Cards",
+    props: {
+        comics: Array
+    }
+}
+
+</script>
+
+<template>
+    <div class="row">
+        <div class="card" v-for="(card, i) in comics" :key="i">
+            <img :src="card.thumb" :alt="card.series">
+            <p>{{ card.series }}</p>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/partials/_variables.scss' as *;
+
+.row {
+    margin-top: 30px;
+    display: flex;
+    flex-wrap: wrap;
+
+    .card {
+        width: calc((100% / 6) - 10px);
+        margin: 0 5px;
+        margin-bottom: 40px;
+
+        img {
+            margin-bottom: 8px;
+        }
+    }
+}
+</style>
